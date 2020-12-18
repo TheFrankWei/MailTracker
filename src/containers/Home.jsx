a9c22fb286d333c45f89510ebd9a46576ec996af
Frank Wei <yw1685@nyu.edu>
2020-12-18 13:07:51 -0500

    working inputfield

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect,} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { makeStyles, TextField } from '@material-ui/core';
+import { makeStyles, TextField, Button } from '@material-ui/core';
 
 //actions
 import { getUspsTracking } from '../actions/UspsActions';
@@ -18,18 +18,38 @@ const Home = () => {
 
     const dispatch = useDispatch();
     const uspsTracking = useSelector(state => state.usps_tracking);
+    
+    const [textInput, setTextInput] = useState('')
 
     useEffect(()=>{
-      dispatch(getUspsTracking('9405509202348003831398'));
+      // dispatch(getUspsTracking('9405509202348003831398'));
     }, []);
+
+    const findTracking = () => {
+      //parse and determine what company tracking to use here
+      dispatch(getUspsTracking(textInput));
+      // dispatch(getUspsTracking('9405509202348003831398'));
+    };
 
     return (
     <div className={classes.home_container}>   
         <h1>MailTracker</h1>
         <form>
-          <TextField variant="outlined" label='Enter your tracking number here!'/>
+          <TextField 
+            variant="outlined" 
+            label='Tracking Number' 
+            placeholder='Input tracking number here!' 
+            value={textInput}
+            onChange={e => setTextInput(e.target.value)}
+          />
+          <br/>
+          <Button variant="contained" color="primary" onClick={findTracking}>
+            Track Package!
+          </Button>
         </form>
         
+
+        {/*display tracking info here */}
     </div>
     );
 };
