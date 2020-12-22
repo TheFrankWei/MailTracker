98f2f8434da4b00dcb6055c86e340eab9ee98d75
Frank Wei <yw1685@nyu.edu>
2020-12-21 19:20:11 -0500

    updated util for xml

--- src/util/AxiosUtil.js
+++ src/util/AxiosUtil.js
@@ -2,15 +2,14 @@ import axios from 'axios';
 
 //need instance for json and xml
 const Instance = {
-  axiosInstance(contentType = 'application/xml') {
+  axiosInstance(contentType = 'text/xml') {
     const instance = axios.create({
-      baseURL: process.env.BASE_URL,
       timeout: 15000,
       headers: this.getHeader(contentType),
     });
     return instance;
   },
-  getHeader(contentType = 'application/xml') {
+  getHeader(contentType = 'text/xml') {
     const API_HEADER = {
       'Content-Type': contentType,
     };
