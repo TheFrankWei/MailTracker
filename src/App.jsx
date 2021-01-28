16c8546fa4c115a4db0fdcee7b472ea981569259
Frank Wei <yw1685@nyu.edu>
2021-01-28 07:49:10 -0500

    working on aws authentication

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/styles';
 import { Provider } from 'react-redux';
-
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import Router from './Router';
 import store from './store/Store';
 import theme from './stylesheets/theme';
@@ -12,9 +12,10 @@ const App = () => (
         <BrowserRouter>
           <ThemeProvider theme={theme}>
             <Router />
+            <AmplifySignOut />
           </ThemeProvider>
         </BrowserRouter>
       </Provider>
 );
 
-export default App;
+export default withAuthenticator(App);
---
+++ src/models/schema.js
@@ -0,0 +1,137 @@
+export const schema = {
+    "models": {
+        "Users": {
+            "name": "Users",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "password": {
+                    "name": "password",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "TrackingNumbers": {
+                    "name": "TrackingNumbers",
+                    "isArray": true,
+                    "type": {
+                        "model": "TrackingNumbers"
+                    },
+                    "isRequired": false,
+                    "attributes": [],
+                    "isArrayNullable": true,
+                    "association": {
+                        "connectionType": "HAS_MANY",
+                        "associatedWith": "untitledmodelID"
+                    }
+                }
+            },
+            "syncable": true,
+            "pluralName": "Users",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                },
+                {
+                    "type": "auth",
+                    "properties": {
+                        "rules": [
+                            {
+                                "allow": "public",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete",
+                                    "read"
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        },
+        "TrackingNumbers": {
+            "name": "TrackingNumbers",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "carrier": {
+                    "name": "carrier",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "tracking_summary": {
+                    "name": "tracking_summary",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "user_notes": {
+                    "name": "user_notes",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "untitledmodelID": {
+                    "name": "untitledmodelID",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                }
+            },
+            "syncable": true,
+            "pluralName": "TrackingNumbers",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                },
+                {
+                    "type": "key",
+                    "properties": {
+                        "name": "byUsers",
+                        "fields": [
+                            "untitledmodelID"
+                        ]
+                    }
+                },
+                {
+                    "type": "auth",
+                    "properties": {
+                        "rules": [
+                            {
+                                "allow": "public",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete",
+                                    "read"
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    },
+    "enums": {},
+    "nonModels": {},
+    "version": "a15e227a8475ebe68c0c2572eef33be5"
+};
